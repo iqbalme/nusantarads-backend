f41eeff0a4b59fda53245dd79db6c89b8ed2bab1
Muhammad Iqbal <webcracking@gmail.com>
2020-07-02 19:02:07 +0800

    fixing shopee trait

--- app/Traits/shopeeTrait.php
+++ app/Traits/shopeeTrait.php
@@ -7,20 +7,22 @@ use App\Model\tQuery;
 use App\Model\tMarketplace;
 use Ixudra\Curl\Facades\Curl;
 use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 trait shopeeTrait
 {
     function shopee_query($q){
-        $query_info = tMarketplace::where('marketplace', 'shopee')->first()->tQuery()->first();
-        $attribut_info = '';
-        $attribut = '';
-        for($i=0;$i<count($q->attribut);$i++){
-            $c_attribut_id = DB::table('t_attribut')->where('nama_attribut', strtoupper(str_replace('_', ' ', key((array)$q->attribut[$i]))))->first()->id;
-            $c_attribut_name = DB::table('t_q_attributes')->where(['marketplace_id' => $query_info->marketplace_id, 'attribut_id' => $c_attribut_id])->first()->attribute_query;
-            $attribut .= '&'.$c_attribut_name.'='.current((array)$q->attribut[$i]);
-        }
-        $c_strcari_id = DB::table('t_attribut')->where('nama_attribut', 'KEYWORD')->first()->id;
-        $strcari = DB::table('t_q_attributes')->where(['marketplace_id' => $query_info->marketplace_id, 'attribut_id' => $c_strcari_id])->first()->attribute_query;
+        // $query_info = tMarketplace::where('marketplace', 'shopee')->first()->tQuery()->first();
+        // $attribut_info = '';
+        // $attribut = '';
+        // for($i=0;$i<count($q->attribut);$i++){
+        //     $c_attribut_id = DB::table('t_attribut')->where('nama_attribut', strtoupper(str_replace('_', ' ', key((array)$q->attribut[$i]))))->first()->id;
+        //     $c_attribut_name = DB::table('t_q_attributes')->where(['marketplace_id' => $query_info->marketplace_id, 'attribut_id' => $c_attribut_id])->first()->attribute_query;
+        //     $attribut .= '&'.$c_attribut_name.'='.current((array)$q->attribut[$i]);
+        // }
+        // $c_strcari_id = DB::table('t_attribut')->where('nama_attribut', 'KEYWORD')->first()->id;
+        // $strcari = DB::table('t_q_attributes')->where(['marketplace_id' => $query_info->marketplace_id, 'attribut_id' => $c_strcari_id])->first()->attribute_query;
         // $shopee_q = $query_info->api_host.$query_info->query.$attribut.'&'.$strcari.'='.urlencode($q->strcari);
         $shopee_q = 'https://shopee.co.id/api/v2/search_items/';
         // $shopee_q = 'https://shopee.co.id/api/v2/search_items/?by=relevancy&keyword='.$strcari.'&limit=100&newest=0&order=desc&page_type=search&version=2';
@@ -33,10 +35,19 @@ trait shopeeTrait
         // } catch(Exception $e){
         //     $response = ['status' => 'error', 'message' => $e];
         // }
-        return response()->json(Curl::to($shopee_q)->withData( array( 'newest' => 0, 'limit' => 100, 'keyword' => 'baju+pria', 'by' => 'relevancy', 'order' => 'desc', 'page_type' => 'search', 'version' => 2 ) )
-        ->withHeaders( array( 'user-agent : Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36', 'upgrade-insecure-requests: 1' ) )
-        ->asJson()->get());
-        // return $response;
+        // return response()->json(Curl::to($shopee_q)->withData( array( 'newest' => 0, 'limit' => 100, 'keyword' => 'blender', 'by' => 'relevancy', 'order' => 'desc', 'page_type' => 'search', 'version' => 2 ) )
+        // ->withHeaders( array( 
+        //     'user-agent : Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        //     'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        //     'connection: keep-alive',
+        //     'server: SGW',
+        // ) )
+        // ->asJson()->get());
+        // return response()->json(Curl::to('https://shopee.co.id/api/v2/search_items/?by=relevancy&keyword=kaos%20wanita&limit=150&newest=0&order=desc&page_type=search&version=2')->asJson()->get());
+        $client = new Client();
+        // Send a request to https://foo.com/api/test
+        $response = $client->request('GET', 'https://shopee.co.id/api/v2/search_items/?by=relevancy&keyword=kaos%20wanita&limit=150&newest=0&order=desc&page_type=search&version=2');
+        return $response;
         // die;
         // return response()->json($response);
     }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/search', 'mainController@search'); // get search result
+Route::get('/search', 'mainController@search'); // get search result
